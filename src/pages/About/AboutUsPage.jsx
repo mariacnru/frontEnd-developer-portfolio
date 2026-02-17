d1158955c74779e31e2e39a8467e25cdfb36bf1c
maria <mariacnru@gmail.com>
2026-02-17 14:35:00 +0330

    AboutPage completed

--- src/pages/About/AboutUsPage.jsx
+++ src/pages/About/AboutUsPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Hero from "./components/Hero";
+import Philosophy from "./components/Philosophy";
+import FutureGoals from "./components/FutureGoals";
 
 function AboutUsPage() {
   return (
-    <div>AboutUsPage</div>
-  )
+    <div className="mx-5 md:mx-20 my-10">
+      <Hero />
+      <Philosophy />
+      <FutureGoals />
+    </div>
+  );
 }
 
-export default AboutUsPage
+export default AboutUsPage;
