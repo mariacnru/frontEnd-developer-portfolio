5f29a6082d6ab2754a7dc3293887b507540d9fda
maria <mariacnru@gmail.com>
2026-02-21 16:43:18 +0330

    contactWays card's icon dark mode fixed

--- src/pages/Contact/components/ContactWays.jsx
+++ src/pages/Contact/components/ContactWays.jsx
@@ -1,51 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../../../context/Theme";
 
 function ContactWays() {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <div className="ContactWays">
       <div className="card">
-        <div className="">
-          <span className="block text-gray-500 text-sm">
-            ارتباط از طریق ایمیل
-          </span>
+        <div>
+          <span className="card-span">ارتباط از طریق ایمیل</span>
           <a href="mail:mariacnru@gmail.com" className="text-sm">
             mariacnru@gmail.com
           </a>
         </div>
         <img
-          src="/public/SVGIcons/arrow-narrow-left.svg"
+          src={
+            theme === "light"
+              ? "/SVGIcons/arrow-narrow-left.svg"
+              : "/SVGIcons/arrow-narrow-left-white.svg"
+          }
           alt="icon"
-          className=""
         />
       </div>
 
       <div className="card">
-        <div className="">
-          <span className="block text-gray-500 text-sm">
-            ارتباط از طریق شماره تماس
-          </span>
+        <div>
+          <span className="card-span">ارتباط از طریق شماره تماس</span>
           <a href="tel:09336049409" className="text-sm">
             0933 60 49 409
           </a>
         </div>
         <img
-          src="/public/SVGIcons/arrow-narrow-left.svg"
+          src={
+            theme === "light"
+              ? "/SVGIcons/arrow-narrow-left.svg"
+              : "/SVGIcons/arrow-narrow-left-white.svg"
+          }
           alt="icon"
-          className=""
         />
       </div>
 
       <div className="card">
-        <div className="">
-          <span className="block text-gray-500 text-sm">موقعیت مکانی </span>
+        <div>
+          <span className="card-span">موقعیت مکانی </span>
           <a href="mail:mariacnru@gmail.com" className="text-sm">
             ایران,تهران{" "}
           </a>
         </div>
         <img
-          src="/public/SVGIcons/arrow-narrow-left.svg"
+          src={
+            theme === "light"
+              ? "/SVGIcons/arrow-narrow-left.svg"
+              : "/SVGIcons/arrow-narrow-left-white.svg"
+          }
           alt="icon"
-          className=""
         />
       </div>
     </div>
