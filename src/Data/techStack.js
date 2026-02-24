3c04fbf6c497df7c1f7707e4477c15a197abf32a
maria <mariacnru@gmail.com>
2026-02-24 11:05:28 +0330

    techStack data added

---
+++ src/Data/techStack.js
@@ -0,0 +1,42 @@
+export const techStack = [
+  {
+    category: "Core",
+    items: ["HTML5", "CSS3", "JavaScript (ES6)"],
+  },
+  {
+    category: "Frameworks",
+    items: ["React.js", "Tailwind CSS"],
+  },
+  {
+    category: "API Architecture",
+    items: ["REST"],
+  },
+  {
+    category: "HTTP Methods",
+    items: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  },
+  {
+    category: "Data Format",
+    items: ["JSON"],
+  },
+  {
+    category: "Client Side",
+    items: ["fetch"],
+  },
+  {
+    category: "Validation",
+    items: ["Zod"],
+  },
+  {
+    category: "Data Visualization",
+    items: ["Recharts"],
+  },
+  {
+    category: "Tools",
+    items: ["Git", "GitHub", "VS Code", "Postman"],
+  },
+  {
+    category: "Databases",
+    items: ["Firebase"],
+  },
+];
